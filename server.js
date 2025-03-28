1ff9453641d787727af4f8350ae60ef6ee1714ba
MARGAM VINAY <margamvinay77@gmail.com>
2025-03-28 09:34:57 +0530

    server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import draftRouter from "./routes/draftRoute.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/", authRouter);
